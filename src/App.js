1ba9a18fe9c32fc5cfaa6e0d4392a7fc5420dc2e
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-05-09 18:14:34 +0400

    Add register page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <LoginForm /> */}
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/Register" element={<RegisterForm />} />
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,10 +1,67 @@
 import React from "react";
-import "../style/registerForm.css"
+import "../style/registerForm.css";
 const RegisterForm = () => {
   return (
     <div className="registerPage">
-      <form className="registerForm">
-        <h1>Register PAge</h1>
+      <form class="form-detail" action="#">
+        <div class="form-row">
+          <div className="fullname">
+            <div className="name">
+              <label for="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                class="input-text"
+                placeholder="Name"
+              />
+            </div>
+            <div>
+              <label for="lastname">Lastname</label>
+              <input
+                type="text"
+                name="lastname"
+                id="name"
+                class="input-text"
+                placeholder="Lastname"
+              />
+            </div>
+          </div>
+        </div>
+        <br />
+        <div class="form-row">
+          <label for="your-email">Your Email</label>
+          <input
+            type="text"
+            name="your-email"
+            id="your-email"
+            class="input-text"
+            placeholder="Email"
+            required=""
+            pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}"
+          />
+        </div>
+        <br />
+        <div class="form-row">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            class="input-text"
+            placeholder="Password"
+            required=""
+          />
+        </div>
+        <br />
+        <div class="form-row-last">
+          <input
+            type="submit"
+            name="register"
+            class="register"
+            value="Register"
+          />
+        </div>
       </form>
     </div>
   );
