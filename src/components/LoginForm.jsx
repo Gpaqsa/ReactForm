43c005b2fddcadec654e4885a383ca6b66c8d7a3
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-05-10 13:44:13 +0400

    Completing the LogIn component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,18 +3,23 @@ import "../style/loginForm.css";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
-  // const [userData, setUserData] = useState({
-
-  // })
-
-  const [logInEmail, setLogInEmail] = useState("");
-  const [logInPassword, setLogInPassaword] = useState("");
+  const [userData, setUserData] = useState({
+    password: "",
+    email: "",
+  });
+  const [submited, setSubmited] = useState(false);
+  const [valid, setValid] = useState(false);
 
   const logInSubmit = (e) => {
     e.preventDefault();
 
-    setLogInEmail("");
-    setLogInPassaword("");
+    if (userData.email && userData.password) {
+      setValid(true);
+    }
+
+    if (userData) {
+      setSubmited(true);
+    }
   };
   return (
     <div className="loginPage">
@@ -22,27 +27,38 @@ const LoginForm = () => {
         <div className="">
           <label htmlFor="email">Email</label>
           <input
-            // className={`${!submited && logInEmail ? "erorrInputActive" : ""}`}
+            className={`${
+              submited && !userData.email ? "erorrInputActive" : ""
+            }`}
             type="email"
             placeholder="youremail@gmail.com"
             onChange={(e) => {
-              setLogInEmail(e.target.value);
-              console.log(logInEmail);
+              setUserData({ ...userData, email: e.target.value });
+              console.log(userData.email);
             }}
-            value={logInEmail}
+            value={userData.email}
           />
+          {submited && !userData.email ? (
+            <span>Please enter your Fullname</span>
+          ) : null}
         </div>
         <div className="">
           <label htmlFor="password">Password</label>
           <input
+            className={`${
+              submited && !userData.password ? "erorrInputActive" : ""
+            }`}
             type="password"
             placeholder="Password"
             onChange={(e) => {
-              setLogInPassaword(e.target.value);
-              console.log(logInPassword);
+              setUserData({ ...userData, password: e.target.value });
+              console.log(userData.password);
             }}
-            value={logInPassword}
+            value={userData.password}
           />
+          {submited && !userData.password ? (
+            <span>Please enter your Fullname</span>
+          ) : null}
         </div>
       </form>
       <div className="buttons">
