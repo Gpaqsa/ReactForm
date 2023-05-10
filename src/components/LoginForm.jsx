1b610b9856c11e83a3be02afd183462af3f1affe
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-05-10 13:02:02 +0400

    Edit loginPage

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/loginForm.css";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
+  // const [userData, setUserData] = useState({
+
+  // })
+
+  const [logInEmail, setLogInEmail] = useState("");
+  const [logInPassword, setLogInPassaword] = useState("");
+
+  const logInSubmit = (e) => {
+    e.preventDefault();
+
+    setLogInEmail("");
+    setLogInPassaword("");
+  };
   return (
     <div className="loginPage">
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={logInSubmit}>
         <div className="">
           <label htmlFor="email">Email</label>
-          <input type="email" placeholder="youremail@gmail.com" />
+          <input
+            // className={`${!submited && logInEmail ? "erorrInputActive" : ""}`}
+            type="email"
+            placeholder="youremail@gmail.com"
+            onChange={(e) => {
+              setLogInEmail(e.target.value);
+              console.log(logInEmail);
+            }}
+            value={logInEmail}
+          />
         </div>
         <div className="">
           <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setLogInPassaword(e.target.value);
+              console.log(logInPassword);
+            }}
+            value={logInPassword}
+          />
         </div>
       </form>
       <div className="buttons">
-        <button className="logBtn">Log In</button>
+        <button className="logBtn" onClick={logInSubmit}>
+          Log In
+        </button>
         <div className="pageButons">
           <button>Forget Password</button>
           <button>
