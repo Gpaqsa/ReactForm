144182d54e96e69bccc0832c80a04634c3a1f5a4
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-05-10 14:21:39 +0400

    Completing the Register  component

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/registerForm.css";
 const RegisterForm = () => {
+  const [values, setValues] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+  const [submited, setSubmited] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleRegSubmit = (event) => {
+    event.preventDefault();
+
+    if (values.name && values.lastname && values.email && values.password) {
+      setValid(true);
+    }
+
+    if (values) {
+      setSubmited(true);
+    }
+  };
+
   return (
     <div className="registerPage">
-      <form className="form-detail" action="#">
+      <form className="form-detail" action="#" onSubmit={handleRegSubmit}>
         <div className="form-row">
           <div className="fullname">
             <div className="name">
@@ -12,19 +33,37 @@ const RegisterForm = () => {
                 type="text"
                 name="name"
                 id="name"
-                className="input-text"
+                className={`"input-text" ${
+                  submited && !values.name ? "erorrInputActive" : ""
+                }`}
                 placeholder="Name"
+                value={values.name}
+                onChange={(e) => {
+                  setValues({ ...values, name: e.target.value });
+                }}
               />
+              {submited && !values.name ? (
+                <span className="nameSpan">Please enter your firstname</span>
+              ) : null}
             </div>
             <div>
               <label htmlFor="lastname">Lastname</label>
               <input
                 type="text"
                 name="lastname"
-                id="name"
-                className="input-text"
+                id="lastname"
+                className={`"input-text" ${
+                  submited && !values.lastname ? "erorrInputActive" : ""
+                }`}
                 placeholder="Lastname"
+                value={values.lastname}
+                onChange={(e) => {
+                  setValues({ ...values, lastname: e.target.value });
+                }}
               />
+              {submited && !values.lastname ? (
+                <span className="lastnameSpan">Please enter your lastname</span>
+              ) : null}
             </div>
           </div>
         </div>
@@ -35,11 +74,21 @@ const RegisterForm = () => {
             type="text"
             name="your-email"
             id="your-email"
-            className="input-text"
+            // className="input-text"
+            className={`"input-text" ${
+              submited && !values.email ? "erorrInputActive" : ""
+            }`}
             placeholder="Email"
-            required=""
+            // required=""
             pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}"
+            value={values.email}
+            onChange={(e) => {
+              setValues({ ...values, email: e.target.value });
+            }}
           />
+          {submited && !values.email ? (
+            <span>Please enter your email</span>
+          ) : null}
         </div>
         <br />
         <div className="form-row">
@@ -48,10 +97,19 @@ const RegisterForm = () => {
             type="password"
             name="password"
             id="password"
-            className="input-text"
+            className={`"input-text" ${
+              submited && !values.password ? "erorrInputActive" : ""
+            }`}
             placeholder="Password"
             required=""
+            value={values.password}
+            onChange={(e) => {
+              setValues({ ...values, password: e.target.value });
+            }}
           />
+          {submited && !values.password ? (
+            <span>Please enter your password</span>
+          ) : null}
         </div>
         <br />
         <div className="form-row-last">
